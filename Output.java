407c96ac96290d5d3e6f40714f4a5a729afa479a
hendychua <hendychua@gmail.com>
2013-08-24 03:12:11 +0800

    Provided option to allow user to choose how to display results. when the output is going to print the results, it will prompt user for his choice: 0 - print to terminal, 1 -  write to results.txt

--- Output.java
+++ Output.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Output
 {
     private Alphabetizer alphabetizer;
+    private final String RESULTS_FILE = "results.txt";
     
     public Output(Alphabetizer alphabetizer)
     {
@@ -11,11 +19,51 @@ public class Output
         LineStorage ls = this.alphabetizer.getLines();
         
         if (ls != null) {
-            System.out.println("**** Results are: ****");
-            for (int i=0; i<ls.length(); i++) {
-                System.out.println(ls.getLineAtIndex(i));
+            System.out.println("Choose how you want to view the results:");
+            System.out.println("0. Print results on terminal.");
+            System.out.println("1. Write results to " + this.RESULTS_FILE + ".");
+            System.out.print("Enter the index to indicate your choice: ");
+            Scanner sc = new Scanner(System.in);
+            String line = sc.nextLine();
+            
+            while (!line.equals("0") && !line.equals("1")) {
+                System.out.print("Please enter 0 or 1: ");
+                line = sc.nextLine();
             }
-            System.out.println("**** End of Results: ****");
+            
+            if (line.equals("0")) {
+                this.printToTerminal(ls);
+            } else if (line.equals("1")) {
+                this.writeToFile(ls);
+            }
         }
+    }
+    
+    private void printToTerminal(LineStorage ls) {
+        System.out.println("**** Results ****");
+        for (int i=0; i<ls.length(); i++) {
+            System.out.println(ls.getLineAtIndex(i));
+        }
+        System.out.println("**** End of Results ****");
+    }
+    
+    private void writeToFile(LineStorage ls) {
+        System.out.println("**** Writing to file " + this.RESULTS_FILE + ". ****");
+        Writer writer = null;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                                            new FileOutputStream(this.RESULTS_FILE), "utf-8"));
+            for (int i=0; i<ls.length(); i++) {
+                writer.write(ls.getLineAtIndex(i) + "\n");
+            }
+        } catch (IOException ex){
+            System.out.println("There was an error writing to the file: " + ex.getMessage());
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception ex) {
+            }
+        }
+        System.out.println("**** Finished writing to file " + this.RESULTS_FILE + ". ****");
     }
 }
