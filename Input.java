85e77a139fd11162e94884a36abb8b3d09f8c1da
inian <inian1234@gmail.com>
2013-08-25 19:07:58 +0800

    Support for reading input from file

--- Input.java
+++ Input.java
@@ -1,12 +1,61 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Input
 {
+	public final String INPUT_FILE = "input.txt";
     public Input() {
     }
     
     public LineStorage parseLines() {
          // returns the data structure LineStorage which contains lines entered by user
+    	
+    	LineStorage lines = null;
+    	System.out.println("Enter the mode of input: ");
+    	System.out.println("0. Read from " + INPUT_FILE);
+    	System.out.println("1. Enter the input in the terminal window.");
+    	
+    	Scanner sc = new Scanner(System.in);
+    	String line = sc.nextLine();
+    	while(!line.equals("0") && !line.equals("1")) {
+    		System.out.println("Please enter 0 or 1:");
+    		line = sc.nextLine();
+    	}
+    	
+    	if(line.equals("0")) {
+    		lines = readInputFromFile();
+    	} else if(line.equals("1")) {
+    		lines = readInputFromTerminal();
+    	}
+    	return lines;
+    }
+    
+    private LineStorage readInputFromFile() {
+    	LineStorage lines = new LineStorage();
+    	String line = new String();
+    	System.out.println("reading from file");
+    	File inputFile = new File(INPUT_FILE);
+    	BufferedReader reader = null;
+    	try {
+			reader = new BufferedReader(new FileReader(inputFile));
+			line = reader.readLine();
+			while(line != null) {
+				lines.addLine(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("There was an error reading from the file: " + e.getMessage());
+		}
+		return lines;
+	}
+
+	private LineStorage readInputFromTerminal() {
+		// TODO Auto-generated method stub
     	LineStorage lines = new LineStorage();
     	System.out.println("Enter movie titles");
     	Scanner sc = new Scanner(System.in);
@@ -16,12 +65,12 @@ public class Input
     		line = sc.nextLine();
     	}
         return lines;
-    }
-    
-    public ForbiddenWords parseWords() {
+	}
+
+	public ForbiddenWords parseWords() {
         // returns the data structure ForbiddenWords which contains words to ignore entered by user
     	ForbiddenWords forbiddenWords = new ForbiddenWords();
-    	System.out.println("Enter the forbidden words");
+    	System.out.println("Enter the forbidden words");	
     	Scanner sc = new Scanner(System.in);
     	String word = sc.next();
     	while(!word.equals("0")) {
